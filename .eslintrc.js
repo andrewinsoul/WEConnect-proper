30063320a1d327fed677a1c1f47c2c8b0486a276
Andrew Okoye <andrewinsoul@gmail.com>
2018-04-03 17:16:09 -0700

    turn off some rules in the .eslintrc.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    "rules": {
+        "undefined:undefined": 0,
+        "import/no-named-as-default-member": 0,
+        "import/no-named-as-default": 0,
+        "import/prefer-default-export": 0,
+        "max-len": 0,
+        "no-console": 0,
+         
+    }
 };
