c52772ef385c61e1ec3e91b7ea2706722f49113b
Andrew Okoye <andrewinsoul@gmail.com>
2018-04-03 17:17:53 -0700

    modify the entry file by importing the router from the route file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import router from './server/routes/route';
 
 const app = express();
 
@@ -9,6 +10,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/v1', router);
 
-const port = parseInt(process.env.PORT, 10) || 8000;
+const PORT = parseInt(process.env.PORT, 10) || 8000;
 
 app.listen(PORT, () => console.log('server live on port 8000'));
