201c5e2c5eb972af52fc94a867e24ab63ac6816c
Andrew Okoye <andrewinsoul@gmail.com>
2018-05-17 14:44:58 -0700

    add the bin folder which holds the www.js file that is responsible for booting the server

---
+++ server/bin/www.js
@@ -0,0 +1,8 @@
+import http from 'http';
+import app from '../app';
+
+const port =  parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+const server = http.createServer(app);
+console.log('site launched via express');
+server.listen(port);
