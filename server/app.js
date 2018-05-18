96521c31544e01969748b922621eefd8d00e5fae
Andrew Okoye <andrewinsoul@gmail.com>
2018-05-18 02:47:10 -0700

    modify app.js file by deleting the import routes line

--- server/app.js
+++ server/app.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-import router from './routes/routes';
 
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/api/v1', router);
 export default app;
