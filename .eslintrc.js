cfcc3bb75b8ebde0738b63bb658a50f9d9dc955b
Andrew Okoye <andrewinsoul@gmail.com>
2018-05-18 14:01:09 -0700

    disable the consistent-return in the eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,10 @@ module.exports = {
         "brace-style": 0,
         "import/no-named-as-default-member": 0,
         "quotes": 0,
+        "no-plusplus": 0,
+        "no-continue": 0,
+        "consistent-return": 0,
+        
         
         
     }
