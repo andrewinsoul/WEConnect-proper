a626b19914f64b7576bf78e0dd77e2d9a6b3e61f
Andrew Okoye <andrewinsoul@gmail.com>
2018-05-19 05:04:37 -0700

    import the route files and expressValidator into the express app

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import ExpressValidator from 'express-validator';
+import { businessRouter } from './routes/business-routes';
+import { reviewRouter } from './routes/review-routes';
+import { userRouter } from './routes/user-routes';
 
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(ExpressValidator());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api/v1', businessRouter);
+app.use('/api/v1', reviewRouter);
+app.use('/api/v1', userRouter);
+
 export default app;
