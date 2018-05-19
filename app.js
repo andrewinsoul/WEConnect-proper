f2f63dec203d5b7d47af8209e079b6a7cacbcc42
Andrew Okoye <andrewinsoul@gmail.com>
2018-05-19 13:42:42 -0700

    install swagger-ui-express and yamljs in the app to publish documentation

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import router from './server/routes/route';
 
 const app = express();
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 app.use(logger('dev'));
+app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/v1', router);
