580ec6ffb6e9420ccf4d2ba9e8907528c076a813
Andrew Okoye <andrewinsoul@gmail.com>
2018-05-27 09:51:10 -0700

    modify package.json

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ module.exports = {
     dialect: "postgres",
   },
   test: {
-    use_env_variable: 'DB_URL_TEST',
+    use_env_variable: 'postgres://@localhost/test',
   },
   production: {
     use_env_variable: 'DB_URL_PROD',
