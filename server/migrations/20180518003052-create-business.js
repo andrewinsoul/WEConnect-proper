64e90fce046c97d3f30a536f2e8454e60cf77c78
Andrew Okoye <andrewinsoul@gmail.com>
2018-06-11 04:47:31 -0700

    modify business model by removing the business profile picture column

--- server/migrations/20180518003052-create-business.js
+++ server/migrations/20180518003052-create-business.js
@@ -15,10 +15,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      businessPhoto: {
-        type: Sequelize.STRING.BINARY,
-        allowNull: false,
-      },
+      // businessPhoto: {
+      //   type: Sequelize.STRING.BINARY,
+      //   allowNull: false,
+      // },
       location: {
         type: Sequelize.STRING,
         allowNull: false,
